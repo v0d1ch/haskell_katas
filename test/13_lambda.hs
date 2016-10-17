5c0eeec3bee5c04a91ebeb401451d0f344b1efb1
Attila Domokos <adomokos@gmail.com>
2016-10-17 18:23:25 -0500

    Add calculation examples

--- test/13_lambda.hs
+++ test/13_lambda.hs
@@ -10,8 +10,9 @@ main = hspec $ do
             {- [1..4] -}
                 {- `shouldBe` ["odd", "even", "odd", "even"] -}
         it "can take 2 arguments - like normal functions" $ do
+            {- The calculation logic should be: (x * 30 + 3) / y) -}
             pending
-            {- zipWith (*3) / y) [1,2,3] [4,5,6] -}
+            {- [1,2,3] [4,5,6] -}
                 {- `shouldBe` [8.25,12.6,15.5] -}
         it "can pattern match in lambdas" $ do
             pending
