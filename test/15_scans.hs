cd66addb8ad78fd730dc0ef6cf78880fb601f01e
Attila Domokos <adomokos@gmail.com>
2016-10-27 08:36:31 -0500

    Add an example that uses conditional

--- test/15_scans.hs
+++ test/15_scans.hs
@@ -9,3 +9,6 @@ main = hspec $ do
             pending
             [1,2,3] `shouldBe` [0,1,3,6]
             [1,2,3] `shouldBe` [6,5,3,0]
+            {- keep only if they are larger than previous -}
+            [3,4,5,3,7,9,2,1]
+                `shouldBe` [3,4,5,5,7,9,9,9]
