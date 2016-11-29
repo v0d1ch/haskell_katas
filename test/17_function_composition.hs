7fc5acc257b9128244b4a2f5ad999d94ec23a23b
Attila Domokos <adomokos@gmail.com>
2016-11-29 08:55:08 -0600

    Provide better exercise description

--- test/17_function_composition.hs
+++ test/17_function_composition.hs
@@ -13,7 +13,7 @@ main = hspec $ do
                 `shouldBe` [-1,-2,-3,-4,-5]
         it "can compose a new function from 3 other functions" $ do
             pending
-            {- negate sum and get the last element -}
+            {- get the tail of the arrays, sum them and negate it -}
             {- [[1..5],[3..6],[1..7]] -}
                 {- `shouldBe` [-14,-15,-27] -}
         it "can calculate the sum of all odd squares <10000" $ do
