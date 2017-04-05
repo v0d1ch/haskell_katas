9ff5acaeed8f8a6f2272cf2446594fe20cf7bac1
Attila Domokos <adomokos@gmail.com>
2017-04-05 12:41:49 -0500

    Ignore code not implemented

--- test/27_derived_instances.hs
+++ test/27_derived_instances.hs
@@ -10,33 +10,38 @@ import Control.Exception (evaluate)
 -}
 
 {- mikeD Michael Diamong 43 -}
-mikeD :: Person
+{- mikeD :: Person -}
 
 {- adRock Adam Horobitz 41 -}
-adRock :: Person
+{- adRock :: Person -}
 
 {- mca Adam Yauch 44 -}
-mca :: Person
+{- mca :: Person -}
 
 main :: IO()
 main = hspec $ do
     describe "Person" $ do
         it "can compare two fields" $ do
-            mca == adRock `shouldBe` False
-            mikeD == adRock `shouldBe` False
-            mikeD /= adRock `shouldBe` True
-            mikeD == mikeD `shouldBe` True
-            mikeD == Person {firstName="Michael",lastName="Diamond",age=43}
-                `shouldBe` True
+            pending
+            {- mca == adRock `shouldBe` False -}
+            {- mikeD == adRock `shouldBe` False -}
+            {- mikeD /= adRock `shouldBe` True -}
+            {- mikeD == mikeD `shouldBe` True -}
+            {- mikeD == Person {firstName="Michael",lastName="Diamond",age=43} -}
+                {- `shouldBe` True -}
         it "will work with `elem` as Person is in Eq" $ do
-            let beastieBoys = [mca, adRock, mikeD]
-            (mikeD `elem` beastieBoys) `shouldBe` True
+            pending
+            {- let beastieBoys = [mca, adRock, mikeD] -}
+            {- (mikeD `elem` beastieBoys) `shouldBe` True -}
         it "is is now an instance of Show" $ do
-            "mikeD is: " ++ show mikeD
-                `shouldBe` "mikeD is: Person {firstName = \"Michael\", lastName = \"Diamond\", age = 43}"
+            pending
+            {- "mikeD is: " ++ show mikeD -}
+                {- `shouldBe` "mikeD is: Person {firstName = \"Michael\", lastName = \"Diamond\", age = 43}" -}
         it "is is now an instance of Read" $ do
-            (firstName $ (read "Person {firstName=\"Michael\",lastName=\"Diamond\",age=43}" :: Person))
-                `shouldBe` "Michael"
+            pending
+            {- (firstName $ (read "Person {firstName=\"Michael\",lastName=\"Diamond\",age=43}" :: Person)) -}
+                {- `shouldBe` "Michael" -}
         it "can infer the type, no type annotation is needed" $ do
-            read "Person {firstName=\"Michael\",lastName=\"Diamond\",age=43}" == mikeD
-                `shouldBe` True
+            pending
+            {- read "Person {firstName=\"Michael\",lastName=\"Diamond\",age=43}" == mikeD -}
+                {- `shouldBe` True -}
