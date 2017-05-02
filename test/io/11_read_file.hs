19d554c23cc1daabc4375453c39f005e9a506bc1
Attila Domokos <adomokos@gmail.com>
2017-05-02 08:25:56 -0500

    Add 3 more examples to test/io

---
+++ test/io/11_read_file.hs
@@ -0,0 +1,11 @@
+{-
+    Reads a file and points out its content
+    Use the file test/io/girlfriend.txt
+-}
+import System.IO
+
+main = do
+    handle <- ___
+    contents <- ___
+    putStr contents
+    ___ handle
---
+++ test/io/12_with_file_read_file.hs
@@ -0,0 +1,8 @@
+{-
+    withFile :: FilePath -> (Handle -> IO a) -> IO a
+    Display the file content from test/io/girlfriend.txt
+-}
+import System.IO
+
+main = do
+    withFile ___
